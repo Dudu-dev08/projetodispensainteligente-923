a22ac0367857fd49d275befde208503c92ef06df
roy <agmr2@aluno.ifal.edu.br>
2026-06-23 16:38:44 -0300

    comentarios

--- lib/dao/notificacao_dao.dart
+++ lib/dao/notificacao_dao.dart
@@ -1,17 +1,17 @@
 import 'package:sqflite/sqflite.dart';
 import '../database/db_helper.dart';
-import '../models/notificacao.dart';
+import '../models/notificacao.dart'; //modelo
 
 class NotificacaoDao {
-  Future<int> inserir(Notificacao notificacao) async {
-    Database db = await DatabaseHelper.instance.database;
+  Future<int> inserir(Notificacao notificacao) async { //devolve número futuro; assincrono; grava notificacao
+    Database db = await DatabaseHelper.instance.database; //conexao banco; espera
     return await db.insert('notificacoes', notificacao.toMap());
   }
 
   Future<List<Notificacao>> listarTodas() async {
     Database db = await DatabaseHelper.instance.database;
-    List<Map<String, dynamic>> maps = await db.query('notificacoes');
-    return List.generate(maps.length, (i) {
+    List<Map<String, dynamic>> maps = await db.query('notificacoes'); //busca
+    return List.generate(maps.length, (i) { //variavel
       return Notificacao.fromMap(maps[i]);
     });
   }
--- lib/models/notificacao.dart
+++ lib/models/notificacao.dart
@@ -4,10 +4,10 @@ class Notificacao {
   String data;
   String mensagem;
 
-  Notificacao({this.id, required this.titulo, required this.data, required this.mensagem});
+  Notificacao({this.id, required this.titulo, required this.data, required this.mensagem}); //construtor da classe
 
-  Map<String, dynamic> toMap() {
-    return {
+  Map<String, dynamic> toMap() { //dicionario de chave e valor; flutter>bd
+    return { //retorna os valores
       'id': id,
       'titulo': titulo,
       'data': data,
@@ -15,7 +15,7 @@ class Notificacao {
     };
   }
 
-  factory Notificacao.fromMap(Map<String, dynamic> map) {
+  factory Notificacao.fromMap(Map<String, dynamic> map) { //inverso da anterior
     return Notificacao(
       id: map['id'],
       titulo: map['titulo'],
